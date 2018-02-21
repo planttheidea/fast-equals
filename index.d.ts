b3e23763e85ab6959fd3de6238feca72d121ad40
Joshua Sachs <josh-sachs@users.noreply.github.com>
2018-02-21 12:58:22 -0500

    Add Typescript Typings

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare type Comparator = (objectA, objectB) => boolean;
+
+export declare function createCustomEqual(createIsEqual?: Comparator): Comparator; 
+export declare function deepEqual(objectA, objectB): boolean;
+export declare function shallowEqual(objectA, objectB): boolean;
+
+export default {
+    createCustom: createCustomEqual,
+    deep: deepEqual,
+    shallow: shallowEqual,
+};
