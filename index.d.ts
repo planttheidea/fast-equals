b1aab456f0c8721b1816e74a8c8844d7542fe86a
Tony Quetano <tony.quetano@planttheidea.com>
2018-05-13 10:41:23 -0400

    Update TS bindings

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,19 @@
-declare type Comparator = (objectA: any, objectB: any) => boolean;
+declare type Comparator = (objectA: any, objectB: any, meta: any) => boolean;
 
 export declare function createCustomEqual(createIsEqual?: Comparator): Comparator;
+export declare function circularDeepEqual(objectA: any, objectB: any): boolean;
+export declare function circularShallowEqual(objectA: any, objectB: any): boolean;
 export declare function deepEqual(objectA: any, objectB: any): boolean;
 export declare function shallowEqual(objectA: any, objectB: any): boolean;
 export declare function sameValueZeroEqual(objectA: any, objectB: any): boolean;
 
 type module = {
-    createCustom: typeof createCustomEqual,
-    deep: typeof deepEqual,
-    shallow: typeof shallowEqual,
-    sameValueZero: typeof sameValueZeroEqual,
+  circularDeep: typeof circularDeepEqual;
+  circularShallow: typeof circularShallowEqual;
+  createCustom: typeof createCustomEqual;
+  deep: typeof deepEqual;
+  shallow: typeof shallowEqual;
+  sameValueZero: typeof sameValueZeroEqual;
 };
 
 export default module;
