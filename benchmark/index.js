574003a1fe8eb2e5fc9762aa0d62b827830627f6
Tony Quetano <tony.quetano@planttheidea.com>
2023-01-30 21:15:28 -0500

    move to `tinybench` for benchmarking

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 import { deepStrictEqual as assertDeepStrictEqual } from 'assert';
-import { createSuite } from 'benchee';
-import Table from 'cli-table3';
 import tests from '../__tests__/__helpers__/testSuites.js';
 
 import deepEql from 'deep-eql';
@@ -10,12 +8,14 @@ import deepEqual from 'deep-equal';
 import fastDeepEqual from 'fast-deep-equal';
 import * as fe from '../dist/esm/index.mjs';
 import isEqualLodash from 'lodash/isEqual.js';
+import sortBy from 'lodash/sortBy.js';
 import nanoEqual from 'nano-equal';
 import reactFastCompare from 'react-fast-compare';
 import shallowEqualFuzzy from 'shallow-equal-fuzzy';
 import { isEqual as isEqualUnderscore } from 'underscore';
+import { Bench } from 'tinybench';
 
-const equalPackages = {
+const packages = {
   'assert.deepStrictEqual': (a, b) => {
     try {
       return assertDeepStrictEqual(a, b);
@@ -44,117 +44,28 @@ const equivalentTests = tests.filter(({ description }) =>
   filteredEquivalentTests.every((test) => !~description.indexOf(test)),
 );
 
-const passingTests = {};
-
 console.log('');
 
-const getPassedKey = (equalName, { description }) =>
-  `${equalName} - ${description}`;
+const getPassedKey = (name, { description }) => `${name} - ${description}`;
 
-const getResults = (results) => {
-  const table = new Table({
-    head: ['Name', 'Ops / sec'],
-  });
+const iterations = 1000;
+const mixedTypesBench = new Bench({ iterations });
+const typesBenches = {};
 
-  results.forEach(({ name, stats }) => {
-    if (!~name.indexOf('passing: false')) {
-      table.push([
-        name.replace(' (passing: true)', ''),
-        stats.ops.toLocaleString(),
-      ]);
-    }
-  });
+for (const name in packages) {
+  const fn = packages[name];
 
-  return table.toString();
-};
-
-const suite = createSuite({
-  minTime: 1000,
-  onComplete(results) {
-    const combinedResults = Object.keys(results)
-      .reduce((combined, group) => {
-        const groupResults = results[group];
-
-        return groupResults.map(({ name, stats }) => {
-          const existingRowIndex = combined.findIndex(
-            ({ name: rowName }) => name === rowName,
-          );
-
-          return ~existingRowIndex
-            ? {
-                ...combined[existingRowIndex],
-                stats: {
-                  elapsed: (combined[existingRowIndex].stats.elapsed +=
-                    stats.elapsed),
-                  iterations: (combined[existingRowIndex].stats.iterations +=
-                    stats.iterations),
-                },
-              }
-            : {
-                name,
-                stats: {
-                  elapsed: stats.elapsed,
-                  iterations: stats.iterations,
-                },
-              };
-        });
-      }, [])
-      .map(({ name, stats }) => ({
-        name,
-        stats: {
-          ...stats,
-          ops: stats.iterations / stats.elapsed,
-        },
-      }))
-      .sort((a, b) => {
-        if (a.stats.ops > b.stats.ops) {
-          return -1;
-        }
-
-        if (a.stats.ops < b.stats.ops) {
-          return 1;
-        }
-
-        return 0;
-      });
-
-    console.log('');
-    console.log('Benchmark results complete, overall averages:');
-    console.log('');
-    console.log(getResults(combinedResults));
-    console.log('');
-  },
-  onGroupComplete({ group, results }) {
-    console.log('');
-    console.log(`...finished group ${group}.`);
-    console.log('');
-    console.log(getResults(results));
-    console.log('');
-  },
-  onGroupStart(group) {
-    console.log('');
-    console.log(`Starting benchmarks for group ${group}...`);
-    console.log('');
-  },
-  onResult({ name, stats }) {
-    console.log(
-      `Benchmark completed for ${name}: ${stats.ops.toLocaleString()} ops/sec`,
-    );
-  },
-});
-
-for (const equalName in equalPackages) {
-  const equalFunc = equalPackages[equalName];
+  const passingTests = {};
 
   for (const testSuite of tests) {
     let passed = true;
 
     for (const test of testSuite.tests) {
       try {
-        if (equalFunc(test.value1, test.value2) !== test.deepEqual) {
+        if (fn(test.value1, test.value2) !== test.deepEqual) {
           console.error(
             'different result',
-            equalName,
+            name,
             testSuite.description,
             test.description,
           );
@@ -162,50 +73,45 @@ for (const equalName in equalPackages) {
           passed = false;
         }
       } catch (e) {
-        console.error(equalName, testSuite.description, test.description, e);
+        console.error(name, testSuite.description, test.description, e);
 
         passed = false;
       }
     }
 
-    passingTests[getPassedKey(equalName, testSuite)] = passed;
+    passingTests[getPassedKey(name, testSuite)] = passed;
   }
 
-  console.log('');
-
-  suite.add(equalName, 'mixed types', () => {
+  mixedTypesBench.add(name, () => {
     for (const testSuite of equivalentTests) {
       for (const test of testSuite.tests) {
         if (
           test.description !== 'pseudo array and equivalent array are not equal'
         ) {
-          equalFunc(test.value1, test.value2);
+          fn(test.value1, test.value2);
         }
       }
     }
   });
-}
 
-for (const testSuite of tests) {
-  for (const equalName in equalPackages) {
-    let equalFunc = equalPackages[equalName];
-
-    if (equalFunc === true) {
-      equalFunc = require(equalName);
+  for (const testSuite of tests) {
+    if (!typesBenches[testSuite.description]) {
+      typesBenches[testSuite.description] = new Bench({ iterations });
     }
 
-    suite.add(
-      `${equalName} (passing: ${
-        passingTests[getPassedKey(equalName, testSuite)]
+    const typesBench = typesBenches[testSuite.description];
+
+    typesBench.add(
+      `${name} (${
+        passingTests[getPassedKey(name, testSuite)] ? 'passed' : 'failed'
       })`,
-      testSuite.description,
       () => {
         for (const test of testSuite.tests) {
           if (
             test.description !==
             'pseudo array and equivalent array are not equal'
           ) {
-            equalFunc(test.value1, test.value2);
+            fn(test.value1, test.value2);
           }
         }
       },
@@ -213,4 +119,26 @@ for (const testSuite of tests) {
   }
 }
 
-suite.run();
+async function run(name, bench) {
+  console.log('');
+  console.log(`Testing ${name}...`);
+
+  await bench.run();
+
+  const tasks = sortBy(bench.tasks, ({ result }) => result.mean).filter(
+    ({ name }) => !name.includes('failed'),
+  );
+
+  console.table(
+    tasks.map(({ name, result }) => ({
+      Package: name.replace(' (passed)', ''),
+      'Ops/sec': +(result?.hz).toFixed(6),
+    })),
+  );
+}
+
+await run('mixed types', mixedTypesBench);
+
+for (const type in typesBenches) {
+  await run(type, typesBenches[type]);
+}
