fd14948aee365d534e6d487c2eec1d950c73d099
Tony Quetano <tony.quetano@planttheidea.com>
2023-01-30 22:57:28 -0500

    add tests for `strictDeepEqual`, specifically testing the issue use-cases

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -6,6 +6,10 @@ import {
   deepEqual,
   sameValueZeroEqual,
   shallowEqual,
+  strictCircularDeepEqual,
+  strictCircularShallowEqual,
+  strictDeepEqual,
+  strictShallowEqual,
 } from '../src';
 import { BaseCircular } from '../src/internalTypes';
 
@@ -17,6 +21,10 @@ describe('exports', () => {
     deepEqual,
     sameValueZeroEqual,
     shallowEqual,
+    strictCircularDeepEqual,
+    strictCircularShallowEqual,
+    strictDeepEqual,
+    strictShallowEqual,
   ].forEach((fn) => {
     it(`should have an export for ${fn.name}`, () => {
       expect(typeof fn).toBe('function');
@@ -83,16 +91,32 @@ describe('test suites', () => {
               expect(deepEqual(value1, value2)).toBe(de);
             });
 
+            it(`should return ${de} for strictDeepEqual comparison of ${testDescription} (test ${testIndex})`, () => {
+              expect(strictDeepEqual(value1, value2)).toBe(de);
+            });
+
             it(`should return ${de} for circularDeepEqual comparison of ${testDescription} (test ${testIndex})`, () => {
               expect(circularDeepEqual(value1, value2)).toBe(de);
+            });
+
+            it(`should return ${de} for strictCircularDeepEqual comparison of ${testDescription} (test ${testIndex})`, () => {
+              expect(strictCircularDeepEqual(value1, value2)).toBe(de);
             });
 
             it(`should return ${se} for shallowEqual comparison of ${testDescription} (test ${testIndex})`, () => {
               expect(shallowEqual(value1, value2)).toBe(se);
             });
 
+            it(`should return ${se} for strictShallowEqual comparison of ${testDescription} (test ${testIndex})`, () => {
+              expect(strictShallowEqual(value1, value2)).toBe(se);
+            });
+
             it(`should return ${se} for circularShallowEqual comparison of ${testDescription} (test ${testIndex})`, () => {
               expect(circularShallowEqual(value1, value2)).toBe(se);
+            });
+
+            it(`should return ${se} for strictCircularShallowEqual comparison of ${testDescription} (test ${testIndex})`, () => {
+              expect(strictCircularShallowEqual(value1, value2)).toBe(se);
             });
           },
         );
@@ -101,116 +125,212 @@ describe('test suites', () => {
   );
 });
 
-describe('circularDeepEqual', () => {
-  it('should handles deeply-nested circular objects', () => {
-    expect(
-      circularDeepEqual(new DeepCircular('foo'), new DeepCircular('foo')),
-    ).toBe(true);
-    expect(
-      circularDeepEqual(new DeepCircular('foo'), new DeepCircular('bar')),
-    ).toBe(false);
+describe('circular', () => {
+  describe('circularDeepEqual', () => {
+    it('should handles deeply-nested circular objects', () => {
+      expect(
+        circularDeepEqual(new DeepCircular('foo'), new DeepCircular('foo')),
+      ).toBe(true);
+      expect(
+        circularDeepEqual(new DeepCircular('foo'), new DeepCircular('bar')),
+      ).toBe(false);
+    });
+
+    it('should handle shared references between objects', () => {
+      const x = [1];
+      const left = [{ a: [1], b: x }];
+      const right = [{ a: x, b: [1] }];
+
+      // should returns true, but returns false
+      expect(circularDeepEqual(left, right)).toBe(true);
+    });
+
+    it('should handle shared circular arrays constructed differently', () => {
+      type RecursiveArray = Array<number | RecursiveArray>;
+      const x: RecursiveArray = [1];
+      x.push(x);
+      const left = [[1, x], x];
+      const right = [x, [1, x]];
+
+      expect(circularDeepEqual(left, right)).toBe(true);
+    });
   });
 
-  it('should handle shared references between objects', () => {
-    const x = [1];
-    const left = [{ a: [1], b: x }];
-    const right = [{ a: x, b: [1] }];
+  describe('circularShallowEqual', () => {
+    it('should handle shallowly-nested circular objects', () => {
+      const a: any[] = ['foo'];
 
-    // should returns true, but returns false
-    expect(circularDeepEqual(left, right)).toBe(true);
+      a.push(a);
+
+      expect(circularShallowEqual(a, ['foo', a])).toBe(true);
+      expect(circularShallowEqual(a, [a])).toBe(false);
+    });
   });
 
-  it('should handle shared circular arrays constructed differently', () => {
-    type RecursiveArray = Array<number | RecursiveArray>;
-    const x: RecursiveArray = [1];
-    x.push(x);
-    const left = [[1, x], x];
-    const right = [x, [1, x]];
+  describe('createCustomCircularEqual', () => {
+    function getFakeWeakMap(): BaseCircular {
+      const entries: [object, object][] = [];
 
-    expect(circularDeepEqual(left, right)).toBe(true);
+      return {
+        delete(key: object) {
+          for (let index = 0; index < entries.length; ++index) {
+            if (entries[index]![0] === key) {
+              entries.splice(index, 1);
+              return true;
+            }
+          }
+
+          return false;
+        },
+
+        get(key: object) {
+          for (let index = 0; index < entries.length; ++index) {
+            if (entries[index]![0] === key) {
+              return entries[index]![1];
+            }
+          }
+
+          return undefined;
+        },
+
+        set(key: object, value: object) {
+          for (let index = 0; index < entries.length; ++index) {
+            if (entries[index]![0] === key) {
+              entries[index]![1] = value;
+              return this;
+            }
+          }
+
+          entries.push([key, value]);
+
+          return this;
+        },
+      };
+    }
+
+    function areRegExpsEqualNoFlagsSupport(a: RegExp, b: RegExp) {
+      return (
+        a.source === b.source &&
+        a.global === b.global &&
+        a.ignoreCase === b.ignoreCase &&
+        a.multiline === b.multiline &&
+        a.unicode === b.unicode &&
+        a.sticky === b.sticky &&
+        a.lastIndex === b.lastIndex
+      );
+    }
+
+    const customDeepEqualCircular = createCustomEqual({
+      circular: true,
+      createCustomConfig: () => ({
+        areRegExpsEqual: areRegExpsEqualNoFlagsSupport,
+      }),
+      createState: () => ({
+        cache: getFakeWeakMap(),
+      }),
+    });
+
+    it('should handle the custom equality check', () => {
+      expect(
+        customDeepEqualCircular(
+          new DeepCircular('foo'),
+          new DeepCircular('foo'),
+        ),
+      ).toBe(true);
+      expect(
+        customDeepEqualCircular(
+          new DeepCircular('foo'),
+          new DeepCircular('bar'),
+        ),
+      ).toBe(false);
+    });
   });
 });
 
-describe('circularShallowEqual', () => {
-  it('should handle shallowly-nested circular objects', () => {
-    const a: any[] = ['foo'];
+describe('strict', () => {
+  describe('strictDeepEqual', () => {
+    it('issue 93 - should handle symbol properties', () => {
+      const symbol = Symbol('foo');
 
-    a.push(a);
+      const object1 = { [symbol]: 'bar' };
+      const object2 = { [symbol]: 'bar' };
+      const object3 = { [symbol]: 'baz' };
 
-    expect(circularShallowEqual(a, ['foo', a])).toBe(true);
-    expect(circularShallowEqual(a, [a])).toBe(false);
-  });
-});
+      expect(strictDeepEqual(object1, object2)).toBe(true);
+      expect(strictDeepEqual(object1, object3)).toBe(false);
+      expect(strictDeepEqual(object2, object3)).toBe(false);
+    });
 
-describe('createCustomCircularEqual', () => {
-  function getFakeWeakMap(): BaseCircular {
-    const entries: [object, object][] = [];
+    it('should handle keys on arrays', () => {
+      type CustomArray = string[] & { key: string };
 
-    return {
-      delete(key: object) {
-        for (let index = 0; index < entries.length; ++index) {
-          if (entries[index]![0] === key) {
-            entries.splice(index, 1);
-            return true;
-          }
-        }
+      const array1 = ['foo', 'bar'] as CustomArray;
+      const array2 = ['foo', 'bar'] as CustomArray;
+      const array3 = ['foo', 'bar'] as CustomArray;
 
-        return false;
-      },
+      array1.key = 'baz';
+      array2.key = 'baz';
+      array3.key = 'quz';
 
-      get(key: object) {
-        for (let index = 0; index < entries.length; ++index) {
-          if (entries[index]![0] === key) {
-            return entries[index]![1];
-          }
-        }
-
-        return undefined;
-      },
-
-      set(key: object, value: object) {
-        for (let index = 0; index < entries.length; ++index) {
-          if (entries[index]![0] === key) {
-            entries[index]![1] = value;
-            return this;
-          }
-        }
-
-        entries.push([key, value]);
-
-        return this;
-      },
-    };
-  }
-
-  function areRegExpsEqualNoFlagsSupport(a: RegExp, b: RegExp) {
-    return (
-      a.source === b.source &&
-      a.global === b.global &&
-      a.ignoreCase === b.ignoreCase &&
-      a.multiline === b.multiline &&
-      a.unicode === b.unicode &&
-      a.sticky === b.sticky &&
-      a.lastIndex === b.lastIndex
-    );
-  }
-
-  const customDeepEqualCircular = createCustomEqual({
-    circular: true,
-    createCustomConfig: () => ({
-      areRegExpsEqual: areRegExpsEqualNoFlagsSupport,
-    }),
-    createState: () => ({
-      cache: getFakeWeakMap(),
-    }),
+      expect(strictDeepEqual(array1, array2)).toBe(true);
+      expect(strictDeepEqual(array1, array3)).toBe(false);
+      expect(strictDeepEqual(array2, array3)).toBe(false);
+    });
   });
 
-  it('should handle the custom equality check', () => {
-    expect(
-      customDeepEqualCircular(new DeepCircular('foo'), new DeepCircular('foo')),
-    ).toBe(true);
-    expect(
-      customDeepEqualCircular(new DeepCircular('foo'), new DeepCircular('bar')),
-    ).toBe(false);
+  describe('createCustomEqual', () => {
+    it('issue 91 - should handle getters on classes', () => {
+      const fakePrivateProperty: string[] = [];
+      let index = 0;
+
+      class ClassWithPrivateJsField {
+        index = index++;
+
+        constructor(a: string) {
+          fakePrivateProperty[this.index] = a;
+        }
+
+        get a() {
+          return fakePrivateProperty[this.index];
+        }
+      }
+
+      const a = new ClassWithPrivateJsField('v1');
+      const b = new ClassWithPrivateJsField('v2');
+
+      const customEqual = createCustomEqual({
+        circular: true,
+        createCustomConfig: ({ areObjectsEqual }) => ({
+          areObjectsEqual: (a, b, state) => {
+            if (!areObjectsEqual(a, b, state)) {
+              return false;
+            }
+
+            const aInstance = a instanceof ClassWithPrivateJsField;
+            const bInstance = b instanceof ClassWithPrivateJsField;
+
+            if (aInstance || bInstance) {
+              return (
+                aInstance &&
+                bInstance &&
+                state.equals(
+                  a.a,
+                  b.a,
+                  undefined,
+                  undefined,
+                  undefined,
+                  undefined,
+                  state,
+                )
+              );
+            }
+
+            return false;
+          },
+        }),
+      });
+
+      expect(customEqual(a, b)).toBe(false);
+    });
   });
 });
