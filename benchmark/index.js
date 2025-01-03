cc6052e322942e83eb51fbb50f816b5834dfdbc3
Tony Quetano <tony.quetano@planttheidea.com>
2025-01-03 10:05:44 -0500

    test against undefined explicitly

--- benchmark/index.js
+++ benchmark/index.js
@@ -18,7 +18,7 @@ import { Bench } from 'tinybench';
 const packages = {
   'assert.deepStrictEqual': (a, b) => {
     try {
-      return assertDeepStrictEqual(a, b);
+      return assertDeepStrictEqual(a, b) === undefined;
     } catch (e) {
       return false;
     }
@@ -88,8 +88,10 @@ for (const name in packages) {
       () => {
         for (const test of testSuite.tests) {
           if (
-            test.description !== 'pseudo array and equivalent array are not equal'
-            && test.description !== 'empty objects with `null` as prototype are equal'
+            test.description !==
+              'pseudo array and equivalent array are not equal' &&
+            test.description !==
+              'empty objects with `null` as prototype are equal'
           ) {
             fn(test.value1, test.value2);
           }
