015927b0bcb9e74946ac4645d33dabeb1c5d0544
Tony Quetano <tony.quetano@planttheidea.com>
2025-07-04 22:02:18 +0200

    fix extensions for mjs and cjs files

--- scripts/apply-type-file-extensions.mjs
+++ scripts/apply-type-file-extensions.mjs
@@ -1,5 +1,5 @@
 import glob from 'fast-glob';
-import { readFileSync, writeFileSync } from 'node:fs';
+import { readFileSync, rmSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
 const IMPORT_STATEMENT_REGEXP =
@@ -8,26 +8,46 @@ const IMPORT_STATEMENT_REGEXP =
 const EXPORT_STATEMENT_REGEXP =
   /export([ \n\t]*(?:[^ \n\t\{\}]+[ \n\t]*,?)?(?:[ \n\t]*\{(?:[ \n\t]*[^ \n\t"'\{\}]+[ \n\t]*,?)+\})?[ \n\t]*)from[ \n\t]*(['"])([^'"\n]+)(?:['"])/g;
 
-const files = glob.sync(
-  join(import.meta.dirname, '..', 'dist', '*', 'types', '*.d.ts'),
-);
+const BASE_FILEPATH = join(import.meta.dirname, '..', 'dist');
 
-function getReplacement(type) {
-  return function (line, dependencies, _quoteType, location) {
-    const extension = line.startsWith(`${type} type`) ? 'd.ts' : 'ts';
+const files = glob.sync(join(BASE_FILEPATH, '*', 'types', '*.d.ts'));
 
-    return `${type}${dependencies}from '${location}.d.ts'`;
+function getReplacement(type, extensionPrefix) {
+  return function (_line, dependencies, _quoteType, location) {
+    return `${type}${dependencies}from '${location}.d.${extensionPrefix}ts'`;
   };
 }
 
-function getReplacedContents(contents) {
+function getReplacedContents(contents, extensionPrefix) {
   return contents
-    .replaceAll(IMPORT_STATEMENT_REGEXP, getReplacement('import'))
-    .replaceAll(EXPORT_STATEMENT_REGEXP, getReplacement('export'));
+    .replaceAll(IMPORT_STATEMENT_REGEXP, getReplacement('import', extensionPrefix))
+    .replaceAll(EXPORT_STATEMENT_REGEXP, getReplacement('export', extensionPrefix));
+}
+
+function getTypeFilePrefix(file) {
+  const relativePath = file.replace(`${BASE_FILEPATH}/`, '');
+  const initialFolder = relativePath.split('/')[0];
+
+  switch (initialFolder) {
+    case 'esm':
+      return 'm';
+
+    case 'cjs':
+      return 'c';
+
+    default:
+      return '';
+  }
 }
 
 for (const file of files) {
   const contents = readFileSync(file, 'utf-8');
+  const extensionPrefix = getTypeFilePrefix(file);
+  const outputFile = extensionPrefix ? file.replace(/\.d\.ts$/, `.d.${extensionPrefix}ts`) : file;
 
-  writeFileSync(file, getReplacedContents(contents), 'utf-8');
+  writeFileSync(outputFile, getReplacedContents(contents, extensionPrefix), 'utf-8');
+
+  if (outputFile !== file) {
+    rmSync(file, { force: true });
+  }
 }
