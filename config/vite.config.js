eb81e239db6e00a1db1053e0005dff0f6c787bf1
Tony Quetano <tony.quetano@planttheidea.com>
2025-11-05 18:52:04 +0100

    remove webpack, and add in eslint plugin

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 export default defineConfig({
+  plugins: [eslintPlugin()],
   root: './dev',
   server: {
     port: 3000,
